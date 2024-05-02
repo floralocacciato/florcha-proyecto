95c145130feda28f0a586067b60a1937226dd198
floralocacciato <florencialocacciato@gmai.com>
2024-05-02 10:58:54 -0300

    creacion de primera interfaz, uso de direcivas e interpolacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: "", component: InicioComponent
+  }, 
+  // carga perezosa-> 1 modulo
+  // loadChildren indica una ruta hija 
+  // ()=> import: ruta de donde viene el modulo
+  // .then: promesa/ funcion asincronica
+  {
+    path:"", loadChildren:()=> import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioComponent } from './inicio/inicio.component';
+// import { InicioComponent } from './inicio/inicio.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // angular material 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar'
+// import {MatIconModule} from '@angular/material/icon';
+// import {MatButtonModule} from '@angular/material/button';
+// import {MatToolbarModule} from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    // InicioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule,
-    MatButtonModule, 
-    MatToolbarModule,
+    // MatIconModule,
+    // MatButtonModule, 
+    // MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
